77fca848d7922a728fc10d218bd7111bb833f493
Jawahar Dath <Jawahar Dath@192.168.0.3>
2020-01-05 23:16:03 -0500

    Added Getters and Setters

--- src/main/java/com/example/vo/AuthorityVo.java
+++ src/main/java/com/example/vo/AuthorityVo.java
@@ -11,5 +11,21 @@ public class AuthorityVo implements GrantedAuthority {
 
     private String userId;
 
-    private String authority;
+    public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+
+	private String authority;
 }
--- src/main/java/com/example/vo/LoginUserVo.java
+++ src/main/java/com/example/vo/LoginUserVo.java
@@ -12,4 +12,41 @@ public class LoginUserVo {
     private boolean isAccountNonLocked;
     private boolean isCredentialsNonExpired;
     private boolean isEnabled;
+    
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isAccountNonExpired() {
+		return isAccountNonExpired;
+	}
+	public void setAccountNonExpired(boolean isAccountNonExpired) {
+		this.isAccountNonExpired = isAccountNonExpired;
+	}
+	public boolean isAccountNonLocked() {
+		return isAccountNonLocked;
+	}
+	public void setAccountNonLocked(boolean isAccountNonLocked) {
+		this.isAccountNonLocked = isAccountNonLocked;
+	}
+	public boolean isCredentialsNonExpired() {
+		return isCredentialsNonExpired;
+	}
+	public void setCredentialsNonExpired(boolean isCredentialsNonExpired) {
+		this.isCredentialsNonExpired = isCredentialsNonExpired;
+	}
+	public boolean isEnabled() {
+		return isEnabled;
+	}
+	public void setEnabled(boolean isEnabled) {
+		this.isEnabled = isEnabled;
+	}
 }
